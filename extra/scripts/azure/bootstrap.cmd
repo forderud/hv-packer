3e884f73519fe6c78141b8870871d850f6fb5b6c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-08 20:14:58 +0100

    Delete virtualbox, azure & oci files to simplify.

--- extra/scripts/azure/bootstrap.cmd
+++
@@ -1,4 +0,0 @@
-net user /add adpacker xBph2L?h
-net localgroup administrators adpacker /add
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Bypass -Force' -Verb RunAs}"
-
--- extra/scripts/virtualbox/sysprep.cmd
+++
@@ -1,3 +0,0 @@
-sc config winrm start=auto
-C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /quiet /shutdown
-
