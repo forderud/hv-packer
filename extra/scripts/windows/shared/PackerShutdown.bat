f4f2aa51edb760bf5ba8a2269ed15b99db7ab396
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-10 15:21:56 +0100

    TEMPORARILY switch from sysprep to shutdown for machine shutdown.
    This makes the build succeed.
    
    .

--- extra/scripts/windows/shared/PackerShutdown.bat
+++ extra/scripts/windows/shared/PackerShutdown.bat
@@ -1,5 +1,6 @@
 ::netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
 
-C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /unattend:C:\Windows\System32\Sysprep\unattend.xml /quiet /shutdown
+::C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /unattend:C:\Windows\System32\Sysprep\unattend.xml /quiet /shutdown
 
-pause
+shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
+::pause
